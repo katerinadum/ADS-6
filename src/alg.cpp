05c8149691e86053e005a5bb55716d51bcdb2992
katerinadum <katya05102002@gmail.com>
2021-06-05 17:47:12 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -10,7 +10,7 @@ BST<std::string> makeTree(const char* filename) {
   std::ifstream file(filename);
   if (!file) {
     std::cout << "File error!" << std::endl;
-    return 0;
+    return tree;
   }
   std::string word;
   char toLowerCase;
